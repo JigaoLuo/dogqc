82f97f7a6ec56e02cf5b718c895013b5cfe60129
Jigao Luo <luojigao@outlook.com>
2021-12-02 15:04:42 +0800

    change the query23: simple groupby.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- query/plans/tpch23.py
+++ query/plans/tpch23.py
@@ -1,14 +1,15 @@
 # select l_linenumber from lineitem group by l_linenumber
-# alg.projection (
-#     [ "l_linenumber"],
-#     alg.aggregation (
-#         [ "l_linenumber" ],
-#         alg.scan ( "lineitem" )
-#     )
-# )
-
-alg.aggregation (
-    [ "l_linenumber" ], # group by ...
-    [],                 # aggragation function: sum, count...
-    alg.scan ( "lineitem" )
+alg.projection (
+    [ "l_linenumber"],
+    [],
+    alg.aggregation (
+        [ "l_linenumber" ],
+        alg.scan ( "lineitem" )
+    )
 )
+
+# alg.aggregation (
+#     [ "l_linenumber" ], # group by ...
+#     [],                 # aggragation function: sum, count...
+#     alg.scan ( "lineitem" )
+# )
