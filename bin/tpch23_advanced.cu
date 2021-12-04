492646cd8e96a14e582c7a2eb4a537b057071305
Jigao Luo <luojigao@outlook.com>
2021-12-04 21:02:54 +0800

    query23: use one kernel.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- bin/tpch23_advanced.cu
+++ bin/tpch23_advanced.cu
@@ -20,96 +20,100 @@ struct apayl2 {
 };
 
 __global__ void krnl_lineitem1(
-    int* iatt5_llinenum, agg_ht<apayl2>* aht2, int* agg1) {
-    int att5_llinenum;
+    int* iatt5_llinenum, agg_ht<apayl2>* aht2, int* agg1, int* nout_result, int* oatt5_llinenum, int* oatt1_countlli) {
+    {
+        /// The first odl kenrel
+        int att5_llinenum;
 
-    int tid_lineitem1 = 0;
-    unsigned loopVar = ((blockIdx.x * blockDim.x) + threadIdx.x);
-    unsigned step = (blockDim.x * gridDim.x);
-    unsigned flushPipeline = 0;
-    int active = 0;
-    while(!(flushPipeline)) {
-        tid_lineitem1 = loopVar;
-        active = (loopVar < 6001215);
-        // flush pipeline if no new elements
-        flushPipeline = !(__ballot_sync(ALL_LANES,active));
-        if(active) {
-            att5_llinenum = iatt5_llinenum[tid_lineitem1];
-        }
-        // -------- aggregation (opId: 2) --------
-        int bucket = 0;
-        if(active) {
-            uint64_t hash2 = 0;
-            hash2 = 0;
+        int tid_lineitem1 = 0;
+        unsigned loopVar = ((blockIdx.x * blockDim.x) + threadIdx.x);
+        unsigned step = (blockDim.x * gridDim.x);
+        unsigned flushPipeline = 0;
+        int active = 0;
+        while(!(flushPipeline)) {
+            tid_lineitem1 = loopVar;
+            active = (loopVar < 6001215);
+            // flush pipeline if no new elements
+            flushPipeline = !(__ballot_sync(ALL_LANES,active));
             if(active) {
-                hash2 = hash ( (hash2 + ((uint64_t)att5_llinenum)));
+                att5_llinenum = iatt5_llinenum[tid_lineitem1];
             }
-            apayl2 payl;
-            payl.att5_llinenum = att5_llinenum;
-            int bucketFound = 0;
-            int numLookups = 0;
-            while(!(bucketFound)) {
-                bucket = hashAggregateGetBucket ( aht2, 12002430, hash2, numLookups, &(payl));
-                apayl2 probepayl = aht2[bucket].payload;
-                bucketFound = 1;
-                bucketFound &= ((payl.att5_llinenum == probepayl.att5_llinenum));
+            // -------- aggregation (opId: 2) --------
+            int bucket = 0;
+            if(active) {
+                uint64_t hash2 = 0;
+                hash2 = 0;
+                if(active) {
+                    hash2 = hash ( (hash2 + ((uint64_t)att5_llinenum)));
+                }
+                apayl2 payl;
+                payl.att5_llinenum = att5_llinenum;
+                int bucketFound = 0;
+                int numLookups = 0;
+                while(!(bucketFound)) {
+                    bucket = hashAggregateGetBucket ( aht2, 12002430, hash2, numLookups, &(payl));
+                    apayl2 probepayl = aht2[bucket].payload;
+                    bucketFound = 1;
+                    bucketFound &= ((payl.att5_llinenum == probepayl.att5_llinenum));
+                }
             }
+            if(active) {
+                atomicAdd(&(agg1[bucket]), ((int)1));
+            }
+            loopVar += step;
         }
-        if(active) {
-            atomicAdd(&(agg1[bucket]), ((int)1));
-        }
-        loopVar += step;
     }
 
-}
+    cudaDeviceSynchronize();
 
-__global__ void krnl_aggregation2(
-    agg_ht<apayl2>* aht2, int* agg1, int* nout_result, int* oatt5_llinenum, int* oatt1_countlli) {
-    int att5_llinenum;
-    int att1_countlli;
-    unsigned warplane = (threadIdx.x % 32);
-    unsigned prefixlanes = (0xffffffff >> (32 - warplane));
+    {
+        /// The second old kernel
+        int att5_llinenum;
+        int att1_countlli;
+        unsigned warplane = (threadIdx.x % 32);
+        unsigned prefixlanes = (0xffffffff >> (32 - warplane));
 
-    int tid_aggregation2 = 0;
-    unsigned loopVar = ((blockIdx.x * blockDim.x) + threadIdx.x);
-    unsigned step = (blockDim.x * gridDim.x);
-    unsigned flushPipeline = 0;
-    int active = 0;
-    while(!(flushPipeline)) {
-        tid_aggregation2 = loopVar;
-        active = (loopVar < 12002430);
-        // flush pipeline if no new elements
-        flushPipeline = !(__ballot_sync(ALL_LANES,active));
-        if(active) {
+        int tid_aggregation2 = 0;
+        unsigned loopVar = ((blockIdx.x * blockDim.x) + threadIdx.x);
+        unsigned step = (blockDim.x * gridDim.x);
+        unsigned flushPipeline = 0;
+        int active = 0;
+        while(!(flushPipeline)) {
+            tid_aggregation2 = loopVar;
+            active = (loopVar < 12002430);
+            // flush pipeline if no new elements
+            flushPipeline = !(__ballot_sync(ALL_LANES,active));
+            if(active) {
+            }
+            // -------- scan aggregation ht (opId: 2) --------
+            if(active) {
+                active &= ((aht2[tid_aggregation2].lock.lock == OnceLock::LOCK_DONE));
+            }
+            if(active) {
+                apayl2 payl = aht2[tid_aggregation2].payload;
+                att5_llinenum = payl.att5_llinenum;
+            }
+            if(active) {
+                att1_countlli = agg1[tid_aggregation2];
+            }
+            // -------- projection (no code) (opId: 3) --------
+            // -------- materialize (opId: 4) --------
+            int wp;
+            int writeMask;
+            int numProj;
+            writeMask = __ballot_sync(ALL_LANES,active);
+            numProj = __popc(writeMask);
+            if((warplane == 0)) {
+                wp = atomicAdd(nout_result, numProj);
+            }
+            wp = __shfl_sync(ALL_LANES,wp,0);
+            wp = (wp + __popc((writeMask & prefixlanes)));
+            if(active) {
+                oatt5_llinenum[wp] = att5_llinenum;
+                oatt1_countlli[wp] = att1_countlli;
+            }
+            loopVar += step;
         }
-        // -------- scan aggregation ht (opId: 2) --------
-        if(active) {
-            active &= ((aht2[tid_aggregation2].lock.lock == OnceLock::LOCK_DONE));
-        }
-        if(active) {
-            apayl2 payl = aht2[tid_aggregation2].payload;
-            att5_llinenum = payl.att5_llinenum;
-        }
-        if(active) {
-            att1_countlli = agg1[tid_aggregation2];
-        }
-        // -------- projection (no code) (opId: 3) --------
-        // -------- materialize (opId: 4) --------
-        int wp;
-        int writeMask;
-        int numProj;
-        writeMask = __ballot_sync(ALL_LANES,active);
-        numProj = __popc(writeMask);
-        if((warplane == 0)) {
-            wp = atomicAdd(nout_result, numProj);
-        }
-        wp = __shfl_sync(ALL_LANES,wp,0);
-        wp = (wp + __popc((writeMask & prefixlanes)));
-        if(active) {
-            oatt5_llinenum[wp] = att5_llinenum;
-            oatt1_countlli[wp] = att1_countlli;
-        }
-        loopVar += step;
     }
 
 }
@@ -229,31 +233,14 @@ int main() {
     {
         int gridsize=920;
         int blocksize=128;
-        krnl_lineitem1<<<gridsize, blocksize>>>(d_iatt5_llinenum, d_aht2, d_agg1);
+        krnl_lineitem1<<<gridsize, blocksize>>>(d_iatt5_llinenum, d_aht2, d_agg1, d_nout_result, d_oatt5_llinenum, d_oatt1_countlli);
     }
     cudaDeviceSynchronize();
-    std::clock_t stop_krnl_lineitem11 = std::clock();
     {
         cudaError err = cudaGetLastError();
         if(err != cudaSuccess) {
             std::cerr << "Cuda Error in krnl_lineitem1! " << cudaGetErrorString( err ) << std::endl;
             ERROR("krnl_lineitem1")
-        }
-    }
-
-    std::clock_t start_krnl_aggregation22 = std::clock();
-    {
-        int gridsize=920;
-        int blocksize=128;
-        krnl_aggregation2<<<gridsize, blocksize>>>(d_aht2, d_agg1, d_nout_result, d_oatt5_llinenum, d_oatt1_countlli);
-    }
-    cudaDeviceSynchronize();
-    std::clock_t stop_krnl_aggregation22 = std::clock();
-    {
-        cudaError err = cudaGetLastError();
-        if(err != cudaSuccess) {
-            std::cerr << "Cuda Error in krnl_aggregation2! " << cudaGetErrorString( err ) << std::endl;
-            ERROR("krnl_aggregation2")
         }
     }
 
@@ -306,8 +293,6 @@ int main() {
     std::clock_t stop_finish3 = std::clock();
 
     printf("<timing>\n");
-    printf ( "%32s: %6.1f ms\n", "krnl_lineitem1", (stop_krnl_lineitem11 - start_krnl_lineitem11) / (double) (CLOCKS_PER_SEC / 1000) );
-    printf ( "%32s: %6.1f ms\n", "krnl_aggregation2", (stop_krnl_aggregation22 - start_krnl_aggregation22) / (double) (CLOCKS_PER_SEC / 1000) );
     printf ( "%32s: %6.1f ms\n", "finish", (stop_finish3 - start_finish3) / (double) (CLOCKS_PER_SEC / 1000) );
     printf ( "%32s: %6.1f ms\n", "totalKernelTime", (stop_totalKernelTime0 - start_totalKernelTime0) / (double) (CLOCKS_PER_SEC / 1000) );
     printf("</timing>\n");
