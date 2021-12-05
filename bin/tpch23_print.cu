7c296c14954220d9a9938bc020a782c1bdd6ae2d
Jigao Luo <luojigao@outlook.com>
2021-12-05 09:04:54 +0800

    query23: add print.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- bin/tpch23_print.cu
+++ bin/tpch23_print.cu
@@ -236,7 +236,7 @@ int main() {
 
     std::clock_t start_krnl_aggregation22 = std::clock();
     {
-        int gridsize=920;
+        int gridsize=(6001215 + 1) / 128;
         int blocksize=128;
         krnl_aggregation2<<<gridsize, blocksize>>>(d_aht2, d_agg1, d_nout_result, d_oatt5_llinenum, d_oatt1_countlli);
     }
