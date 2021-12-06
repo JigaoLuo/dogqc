86d1338a750d91f08007b49cceee25a350789cfc
Jigao Luo <luojigao@outlook.com>
2021-12-06 14:20:04 +0800

    fix shared memory usage
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- bin/tpch23_advanced.cu
+++ bin/tpch23_advanced.cu
@@ -24,7 +24,7 @@ __global__ void krnl_lineitem1(
     int* iatt5_llinenum, int* nout_result, int* oatt5_llinenum, int* oatt1_countlli) {  ///
 
     /// local block memory cache : ONLY FOR A BLOCK'S THREADS!!!
-    constexpr int SHARED_MEMORY_SIZE = 49152;  /// Total amount of shared memory per block:       49152 bytes
+    int SHARED_MEMORY_SIZE = 49152;  /// Total amount of shared memory per block:       49152 bytes
     const int HT_SIZE = 128;
     __shared__ agg_ht<apayl2> aht2[HT_SIZE];  ///
     __shared__ int agg1[HT_SIZE];  ///
