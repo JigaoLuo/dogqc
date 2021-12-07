d72a963dec7c2820f0fffb6a45459cc1b5cadbea
Jigao Luo <luojigao@outlook.com>
2021-12-07 11:39:44 +0800

    add query25: change query.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- query/plans/tpch25.py
+++ query/plans/tpch25.py
@@ -1,9 +1,12 @@
-# select l_linenumber, sum(l_quantity) from lineitem group by l_linenumber
+# select l_linenumber, sum(CAST(l_suppkey AS bigint))
+# from lineitem
+# group by l_linenumber
+# order by l_linenumber
 alg.projection (
-    [ "l_linenumber", "sum_l_quantity"],
+    [ "l_linenumber", "sum_l_suppkey"],
     alg.aggregation (
         [ "l_linenumber" ], # group by ...
-        [( Reduction.SUM, "l_quantity", "sum_l_quantity" )], # aggragation function: sum, count...
+        [( Reduction.SUM, "l_suppkey", "sum_l_suppkey" )], # aggragation function: sum, count...
         alg.scan ( "lineitem" )
     )
 )
