10bc4635cdbb5795c426096406a7b1d588dab353
Jigao Luo <luojigao@outlook.com>
2021-12-09 14:45:53 +0800

    add query27 advanced: larger blocksize.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- bin/tpch27_advanced.cu
+++ bin/tpch27_advanced.cu
@@ -260,10 +260,11 @@ int main() {
 
     std::clock_t start_totalKernelTime0 = std::clock();
     {
-        int gridsize=920;
-        ///int blocksize=128;
+        /// int gridsize=920;
+        /// int blocksize=128;
         int blocksize=128*8;
-        krnl_lineitem1<<<(6001215 + blocksize - 1) / blocksize, blocksize>>>(d_iatt2_lorderke, d_nout_result, d_oatt2_lorderke, d_oatt1_countlor);
+        int gridsize = (6001215 + blocksize - 1) / blocksize;
+        krnl_lineitem1<<<gridsize, blocksize>>>(d_iatt2_lorderke, d_nout_result, d_oatt2_lorderke, d_oatt1_countlor);
     }
     cudaDeviceSynchronize();
     std::clock_t stop_totalKernelTime0 = std::clock();
