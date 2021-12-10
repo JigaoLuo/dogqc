d0088ba63d94e82bdf55a20e21b0a3c8d7ffaa47
Jigao Luo <luojigao@outlook.com>
2021-12-10 22:40:55 +0800

    query27 done.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- bin/tpch27_advanced.cu
+++ bin/tpch27_advanced.cu
@@ -29,7 +29,7 @@ __global__ void krnl_lineitem1(
     int* iatt2_lorderke, int* nout_result, int* oatt2_lorderke, int* oatt1_countlor) {  ///
 
     /// local block memory cache : ONLY FOR A BLOCK'S THREADS!!!
-    const int HT_SIZE = 1280;
+    const int HT_SIZE = 1024;  ///
     __shared__ agg_ht<apayl2> aht2[HT_SIZE];  ///
     __shared__ int agg1[HT_SIZE];  ///
     const int shared_memory_usage = sizeof(aht2) + sizeof(agg1);
@@ -165,11 +165,107 @@ __global__ void krnl_lineitem1(
 
 }
 
+__global__ void krnl_reduce1(  ///
+        int* oatt2_lorderke, int* oatt1_countlor, agg_ht<apayl2>* aht2, int* agg1, int* nout_result) {  ///
+    int att2_lorderke;  ///
+    int att1_countlor;  ///
+
+    int tid_lineitem1 = 0;
+    unsigned loopVar = ((blockIdx.x * blockDim.x) + threadIdx.x);
+    unsigned step = (blockDim.x * gridDim.x);
+    unsigned flushPipeline = 0;
+    int active = 0;
+    while(!(flushPipeline)) {
+        tid_lineitem1 = loopVar;
+        active = (loopVar < *nout_result);  ///
+        // flush pipeline if no new elements
+        flushPipeline = !(__ballot_sync(ALL_LANES,active));
+        if(active) {
+            att2_lorderke = oatt2_lorderke[tid_lineitem1];  ///
+            att1_countlor = oatt1_countlor[tid_lineitem1];  ///
+        }
+        // -------- aggregation (opId: 2) --------
+        int bucket = 0;
+        if(active) {
+            uint64_t hash2 = 0;
+            hash2 = 0;
+            if(active) {
+                hash2 = hash ( (hash2 + ((uint64_t)att2_lorderke)));
+            }
+            apayl2 payl;
+            payl.att2_lorderke = att2_lorderke;
+            int bucketFound = 0;
+            int numLookups = 0;
+            while(!(bucketFound)) {
+                bucket = hashAggregateGetBucket ( aht2, *nout_result * 2, hash2, numLookups, &(payl));  ///
+                apayl2 probepayl = aht2[bucket].payload;
+                bucketFound = 1;
+                bucketFound &= ((payl.att2_lorderke == probepayl.att2_lorderke));
+            }
+        }
+        if(active) {
+            atomicAdd(&(agg1[bucket]), ((int)att1_countlor));  ///
+        }
+        loopVar += step;
+    }
+}
+
+__global__ void krnl_reduce2(
+        agg_ht<apayl2>* aht2, int* agg1, int* n_final_out_result, int* oatt2_lorderke, int* oatt1_countlor, int* nout_result) {
+    int att2_lorderke;  ///
+    int att1_countlor;  ///
+    unsigned warplane = (threadIdx.x % 32);
+    unsigned prefixlanes = (0xffffffff >> (32 - warplane));
+
+    int tid_aggregation2 = 0;
+    unsigned loopVar = ((blockIdx.x * blockDim.x) + threadIdx.x);
+    unsigned step = (blockDim.x * gridDim.x);
+    unsigned flushPipeline = 0;
+    int active = 0;
+    while(!(flushPipeline)) {
+        tid_aggregation2 = loopVar;
+        active = (loopVar < *nout_result * 2);  ///
+        // flush pipeline if no new elements
+        flushPipeline = !(__ballot_sync(ALL_LANES,active));
+        if(active) {
+        }
+        // -------- scan aggregation ht (opId: 2) --------
+        if(active) {
+            active &= ((aht2[tid_aggregation2].lock.lock == OnceLock::LOCK_DONE));
+        }
+        if(active) {
+            apayl2 payl = aht2[tid_aggregation2].payload;
+            att2_lorderke = payl.att2_lorderke;
+        }
+        if(active) {
+            att1_countlor = agg1[tid_aggregation2];
+        }
+        // -------- projection (no code) (opId: 3) --------
+        // -------- materialize (opId: 4) --------
+        int wp;
+        int writeMask;
+        int numProj;
+        writeMask = __ballot_sync(ALL_LANES,active);
+        numProj = __popc(writeMask);
+        if((warplane == 0)) {
+            wp = atomicAdd(n_final_out_result, numProj);  ///
+        }
+        wp = __shfl_sync(ALL_LANES,wp,0);
+        wp = (wp + __popc((writeMask & prefixlanes)));
+        if(active) {
+            oatt2_lorderke[wp] = att2_lorderke;
+            oatt1_countlor[wp] = att1_countlor;
+        }
+        loopVar += step;
+    }
+}
+
 int main() {
     int* iatt2_lorderke;
     iatt2_lorderke = ( int*) map_memory_file ( "mmdb/lineitem_l_orderkey" );
 
     int nout_result;
+    int final_nout_result;  ///
     std::vector < int > oatt2_lorderke(6001215);
     std::vector < int > oatt1_countlor(6001215);
 
@@ -187,10 +283,16 @@ int main() {
     cudaMalloc((void**) &d_iatt2_lorderke, 6001215* sizeof(int) );
     int* d_nout_result;
     cudaMalloc((void**) &d_nout_result, 1* sizeof(int) );
+    int* d_final_nout_result;  ///
+    cudaMalloc((void**) &d_final_nout_result, 1* sizeof(int) );  ///
     int* d_oatt2_lorderke;
     cudaMalloc((void**) &d_oatt2_lorderke, 6001215* sizeof(int) );
     int* d_oatt1_countlor;
     cudaMalloc((void**) &d_oatt1_countlor, 6001215* sizeof(int) );
+    int* d_final_oatt2_lorderke;  ///
+    cudaMalloc((void**) &d_final_oatt2_lorderke, 6001215* sizeof(int) );  ///
+    int* d_final_oatt1_countlor;  ///
+    cudaMalloc((void**) &d_final_oatt1_countlor, 6001215* sizeof(int) );  ///
     cudaDeviceSynchronize();
     {
         cudaError err = cudaGetLastError();
@@ -261,7 +363,7 @@ int main() {
     std::clock_t start_totalKernelTime0 = std::clock();
     {
         /// int gridsize=920;
-        int blocksize=128;
+        int blocksize=1024;  ///
         int gridsize = (6001215 + blocksize - 1) / blocksize;
         krnl_lineitem1<<<gridsize, blocksize>>>(d_iatt2_lorderke, d_nout_result, d_oatt2_lorderke, d_oatt1_countlor);
     }
@@ -275,9 +377,80 @@ int main() {
         }
     }
 
+
+
+
+
+    ///
+    // input: d_oatt2_lorderke, d_oatt1_countlor
     cudaMemcpy( &nout_result, d_nout_result, 1 * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy( oatt2_lorderke.data(), d_oatt2_lorderke, 6001215 * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy( oatt1_countlor.data(), d_oatt1_countlor, 6001215 * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    {
+        cudaError err = cudaGetLastError();
+        if(err != cudaSuccess) {
+            std::cerr << "Cuda Error in cuda memcpy nout_result! " << cudaGetErrorString( err ) << std::endl;
+            ERROR("cuda memcpy out")
+        }
+    }
+
+    /// output size is less than d_nout_result OR nout_result
+    agg_ht<apayl2>* d_aht2;
+    cudaMalloc((void**) &d_aht2, nout_result * 2 * sizeof(agg_ht<apayl2>) );
+    {
+        int gridsize=920;
+        int blocksize=128;
+        initAggHT<<<gridsize, blocksize>>>(d_aht2, nout_result * 2 );
+    }
+    int* d_agg1;
+    cudaMalloc((void**) &d_agg1, nout_result * 2 * sizeof(int) );
+    {
+        int gridsize=920;
+        int blocksize=128;
+        initArray<<<gridsize, blocksize>>>(d_agg1, 0, nout_result * 2 );
+    }
+    {
+        int gridsize=920;
+        int blocksize=128;
+        initArray<<<gridsize, blocksize>>>(d_final_nout_result, 0, 1);
+    }
+    cudaDeviceSynchronize();
+    {
+        cudaError err = cudaGetLastError();
+        if(err != cudaSuccess) {
+            std::cerr << "Cuda Error in cuda mallocHT! " << cudaGetErrorString( err ) << std::endl;
+            ERROR("cuda mallocHT")
+        }
+    }
+
+    std::clock_t start_krnl_reduce1 = std::clock();
+    {
+        int gridsize=920;
+        int blocksize=128;
+        krnl_reduce1<<<gridsize, blocksize>>>(d_oatt2_lorderke, d_oatt1_countlor, d_aht2, d_agg1, d_nout_result);
+    }
+    cudaDeviceSynchronize();
+    std::clock_t stop_krnl_reduce1 = std::clock();
+    {
+        cudaError err = cudaGetLastError();
+        if(err != cudaSuccess) {
+            std::cerr << "Cuda Error in krnl_reduce1! " << cudaGetErrorString( err ) << std::endl;
+            ERROR("krnl_lineitem1")
+        }
+    }
+
+    std::clock_t start_krnl_reduce2 = std::clock();
+    {
+        int gridsize=920;
+        int blocksize=128;
+        krnl_reduce2<<<gridsize, blocksize>>>(d_aht2, d_agg1, d_final_nout_result, d_final_oatt2_lorderke, d_final_oatt1_countlor, d_nout_result);
+    }
+    cudaDeviceSynchronize();
+    std::clock_t stop_krnl_reduce2 = std::clock();
+
+
+    cudaMemcpy( &final_nout_result, d_final_nout_result, 1 * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy( oatt2_lorderke.data(), d_final_oatt2_lorderke, 6001215 * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy( oatt1_countlor.data(), d_final_oatt1_countlor, 6001215 * sizeof(int), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
     {
         cudaError err = cudaGetLastError();
@@ -287,10 +460,19 @@ int main() {
         }
     }
 
+
+
+
+
+
+
     cudaFree( d_iatt2_lorderke);
     cudaFree( d_nout_result);
     cudaFree( d_oatt2_lorderke);
     cudaFree( d_oatt1_countlor);
+    cudaFree( d_aht2);  ///
+    cudaFree( d_agg1);  ///
+    cudaFree( d_final_nout_result);  ///
     cudaDeviceSynchronize();
     {
         cudaError err = cudaGetLastError();
@@ -301,11 +483,11 @@ int main() {
     }
 
     std::clock_t start_finish3 = std::clock();
-    printf("\nResult: %i tuples\n", nout_result);
-    if((nout_result > 6001215)) {
+    printf("\nResult: %i tuples\n", final_nout_result);  ///
+    if((final_nout_result > 6001215)) {  ///
         ERROR("Index out of range. Output size larger than allocated with expected result number.")
     }
-    for ( int pv = 0; ((pv < 10) && (pv < nout_result)); pv += 1) {
+    for ( int pv = 0; ((pv < 10) && (pv < final_nout_result)); pv += 1) {  ///
         printf("l_orderkey: ");
         printf("%8i", oatt2_lorderke[pv]);
         printf("  ");
@@ -314,7 +496,7 @@ int main() {
         printf("  ");
         printf("\n");
     }
-    if((nout_result > 10)) {
+    if((final_nout_result > 10)) {  ///
         printf("[...unsorted...]\n");
     }
     printf("\n");
@@ -322,7 +504,7 @@ int main() {
 
     /// 40 sorted output
     std::map<int, int> ht;
-    for ( int pv = 0; pv < nout_result; pv += 1) {
+    for ( int pv = 0; pv < final_nout_result; pv += 1) {  ///
         ht.emplace(oatt2_lorderke[pv], oatt1_countlor[pv]);
     }
     auto it = ht.begin();
@@ -345,5 +527,7 @@ int main() {
     printf("<timing>\n");
     printf ( "%32s: %6.1f ms\n", "finish", (stop_finish3 - start_finish3) / (double) (CLOCKS_PER_SEC / 1000) );
     printf ( "%32s: %6.1f ms\n", "totalKernelTime", (stop_totalKernelTime0 - start_totalKernelTime0) / (double) (CLOCKS_PER_SEC / 1000) );
+    printf ( "%32s: %6.1f ms\n", "krnl_reduce1", (stop_krnl_reduce1 - start_krnl_reduce1) / (double) (CLOCKS_PER_SEC / 1000) );  ///
+    printf ( "%32s: %6.1f ms\n", "krnl_reduce2", (stop_krnl_reduce2 - start_krnl_reduce2) / (double) (CLOCKS_PER_SEC / 1000) );  ///
     printf("</timing>\n");
 }
