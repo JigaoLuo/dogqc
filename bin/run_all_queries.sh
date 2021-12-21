cda7fe3d022bbffc564ab96fe62370aa51625d62
Jigao Luo <luojigao@outlook.com>
2021-12-21 14:11:33 +0800

    new path
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- bin/run_all_queries.sh
+++ bin/run_all_queries.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tpch_tbl_files=/home/jigao/Desktop/tpch-dbgen
+tpch_tbl_files=/home/jigao/Desktop/tpch-dbgen-sf1
 #tpch_tbl_files=/home/jigao/Desktop/tpch-dbgen-sf10
 # echo $tpch_tbl_files
 python3 ../query/tpch.py $tpch_tbl_files all
--- bin/run_single_query.sh
+++ bin/run_single_query.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tpch_tbl_files=/home/jigao/Desktop/tpch-dbgen
+tpch_tbl_files=/home/jigao/Desktop/tpch-dbgen-sf1
 #tpch_tbl_files=/home/jigao/Desktop/tpch-dbgen-sf10
 
 function run_single_query {
